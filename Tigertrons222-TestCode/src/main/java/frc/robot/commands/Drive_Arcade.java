2c699c6130ce0c8a07603b01a3101d037281d54f
djberry8@ptd.net <djberry8@ptd.net>
2020-01-09 11:29:57 -0500

    added Drive_Arcade

--- Tigertrons222-TestCode/src/main/java/frc/robot/commands/Drive_Arcade.java
+++ Tigertrons222-TestCode/src/main/java/frc/robot/commands/Drive_Arcade.java
@@ -1,31 +1,15 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/**
- * An example command that uses an example subsystem.
- */
 public class Drive_Arcade extends CommandBase {
-    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-    private final Drivetrain m_subsystem;
+  private final Drivetrain m_subsystem;
 
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
-     */
     public Drive_Arcade(Drivetrain subsystem) {
     m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+
+    addRequirements(m_subsystem);
   }
 
   // Called when the command is initially scheduled.
