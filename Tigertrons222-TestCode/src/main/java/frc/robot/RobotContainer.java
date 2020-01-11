43bbf233bb93309fc408907c4d069223d6224e3c
djberry8@ptd.net <djberry8@ptd.net>
2020-01-11 11:48:03 -0500

    Finished drivetrain

--- Tigertrons222-TestCode/src/main/java/frc/robot/RobotContainer.java
+++ Tigertrons222-TestCode/src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.Drive_Arcade;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -24,33 +21,17 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final Drivetrain m_Drivetrain = new Drivetrain();
+  private final Drivetrain m_drivetrain = new Drivetrain();
 
-  private final Drive_Arcade m_autoCommand = new Drive_Arcade(m_Drivetrain);
+  private final Drive_Arcade m_autoCommand = new Drive_Arcade(m_drivetrain);
 
-  XboxController m_driverController = new XboxController(0);
+  public static final XboxController Controller = new XboxController(0);
+	public static final XboxController Controller2 = new XboxController(1);
 
 
   public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
 
-    // Configure default commands
-    // Set the default drive command to split-stick arcade drive
-    m_robotDrive.setDefaultCommand(
-        // A split-stick arcade command, with forward/backward controlled by the left
-        // hand, and turning controlled by the right.
-        new DefaultDrive(
-            m_robotDrive,
-            () -> m_driverController.getY(GenericHID.Hand.kLeft),
-            () -> m_driverController.getX(GenericHID.Hand.kRight)));
-
-    // Add commands to the autonomous command chooser
-    m_chooser.addOption("Simple Auto", m_simpleAuto);
-    m_chooser.addOption("Complex Auto", m_complexAuto);
-
-    // Put the chooser on the dashboard
-    Shuffleboard.getTab("Autonomous").add(m_chooser);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
