ba4a33c2ab3290106f3dd5cec528cd0d0bea180b
djberry8@ptd.net <djberry8@ptd.net>
2020-02-23 14:34:11 -0500

    yes man

--- Tigertrons222-TestCode/src/main/java/frc/robot/commands/ComplexAuto.java
+++ Tigertrons222-TestCode/src/main/java/frc/robot/commands/ComplexAuto.java
@@ -6,18 +6,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Auto_Rotate;
 import frc.robot.commands.Autodrive_forward;
 
-public class ComplexAuto extends CommandGroupBase {
+public class ComplexAuto extends SequentialCommandGroup {
 
   public void ComplexAuto() {
    addCommands(new Auto_Rotate(5, 45));
-
+   addCommands(new Autodrive_forward(5, 5));
    
-  }
-
-  @Override
-  public void addCommands(Command... commands) {
-    addCommands(new Auto_Rotate(5, 45));
-
   }
 
 
